9f546d4c7e2fedd79bce706b341c519e13761348
Michael Lee <michaelsoolee@gmail.com>
2016-06-28 00:43:03 -0400

    Added in default unit

--- index.js
+++ index.js
@@ -33,6 +33,14 @@ var emojifyWeather = function (icon){
   }
 }
 
+var convertToC = function (fahrenheit){
+  return Math.round((fahrenheit - 32) * 5 / 9)
+}
+
+var convertToF = function (celsius){
+  return Math.round((celsius * 9 / 5) + 32)
+}
+
 var getWeather = function (config, callback){
   var options = {
     host: 'api.forecast.io',
@@ -48,7 +56,8 @@ var getWeather = function (config, callback){
     res.on('end', function(){
       var data = JSON.parse(body).currently
       var icon = emojifyWeather(data.icon)
-      var temp = Math.floor(data.temperature) + '°F'
+      var temp = config.unit === 'F' ? Math.round(data.temperature) : convertToC(data.temperature)
+      temp += '°' + config.unit
       var time = moment.unix(data.time).format('LT')
       var weather = icon + 'It\'s currently ' + temp + ' and ' + data.summary.toLowerCase() + ' at ' + time + '.'
       callback(weather)
@@ -74,13 +83,15 @@ if(configFileExists){
 } else {
   co(function * () {
     var forecastioAPI = yield prompt('What is your forecast.io API key? If you don\'t have one, get one at https://developer.forecast.io: ')
-    var latitude = yield prompt('What is your location\'s latitude? Don\'t know what it is? Get it at http://freegeoip.net: ')
+    var latitude = yield prompt('What is your location\'s latitude? Don\'t know what it is? Get it at https://google-developers.appspot.com/maps/documentation/utils/geocoder/: ')
     var longitude = yield prompt('What is your location\'s longitude? ')
+    var unit = yield prompt('Would you like your temperature in celius or fahrenheit? Please use (C or F): ')
     
     var config = {
       'forecastioAPI': forecastioAPI,
       'latitude': latitude,
-      'longitude': longitude
+      'longitude': longitude,
+      'unit': unit
     }
 
     fs.closeSync(fs.openSync(defaultConfigPath, 'w'))
